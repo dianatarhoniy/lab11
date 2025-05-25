24dc3af5630e07bb5ba1645c280aace66b93723b
s30930 <s30930@pjwstk.edu.pl>
2025-05-25 23:20:45 +0200

    Initial commit

--- src/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ src/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e8b5937116a425770c2685123bcc4b28f8e41a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+def90c9ead8abc8b3cd1b9b2a552c1bc2cb672cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
